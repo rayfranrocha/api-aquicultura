798197b9abf0cc1a1f038b160bc193b4d2929899
silasjr <jose.silasjr.bb@gmail.com>
2018-08-21 12:24:50 -0400

    api de anexo da inscricao

--- app/routes/inscricaoAnexo.js
+++ app/routes/inscricaoAnexo.js
@@ -2,30 +2,6 @@ const mongoose = require('mongoose');
 const InscricaoAnexo = mongoose.model('InscricaoAnexo');
 
 module.exports = (app) => {
-    var api = app.api.inscricao;
-
-    api.methods(['get']);
-    api.register(app, '/inscricaoAnexo');
-    
-    api.before('get', (req, res, nex) => {
-        let param = req.params.idEformato;
-        const [id, formato] = param.split('.');
-
-        if (formato === 'pdf') {
-            InscricaoAnexo.findOne({inscricao: id})
-            .then(anexo => {
-                if (!anexo) {
-                    res.status(404).send('Not found');
-                } else {
-                    res.contentType('application/pdf');
-                    res.send(anexo.file);
-                }
-            });
-        } else {
-            next();
-        }
-
-    });
-    
-
+    var api = app.api.inscricaoAnexo;
+    app.get('/inscricaoAnexo/:idEformato', api.getAnexo);
 }
