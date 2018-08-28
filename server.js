6b40672dcf68fc243cf28d28358399c83db451e6
silas <jose.silasjr.bb@gmail.com>
2018-08-28 15:11:25 -0400

    alterado para banco de dados de teste

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var app = require('./config/express');
 
-var database = process.env.DEV ? 'mongodb://localhost:27017/aquicultura' : 'mongodb://icon:iconmain123@ds119442.mlab.com:19442/aquicultura';
+var database = process.env.DEV ? 'mongodb://localhost:27017/aquicultura' : 'mongodb://icon:iconmain123@ds139939.mlab.com:39939/aquicultura-2';
 require('./config/database')(database);
 
 var port = process.env.PORT || 5000;
