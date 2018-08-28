ac0ac6cb783598e9d2acca1c7b03728ba633ed41
silasjr <jose.silasjr.bb@gmail.com>
2018-08-28 13:11:51 -0400

    validacao quantidade de trabalhos por usuario

--- app/api/trabalhos.js
+++ app/api/trabalhos.js
@@ -3,21 +3,31 @@ const mongoose = require('mongoose');
 const Trabalho = mongoose.model('Trabalho');
 const TrabalhoAnexo = mongoose.model('TrabalhoAnexo');
 
+const MAXIMO_TRABALHOS_POR_USUARIO = 2;
+
 module.exports = function (app) {
     return api = {
         save: (req, res) => {
             // validar para até dois trabalhos por usuario
-            Trabalho.create(req.body)
-                .then(trabalho => {
-                    if (req.files && req.files.file) {
-                        let file = req.files.file;
-                        TrabalhoAnexo.create({file: file.data, contentType: file.mimetype, trabalho})
-                            .then(trabalhoAnexo => {
-                                res.status(201).json(trabalho);
-                            });
-                    } else {
-                        res.status(201).json(trabalho);
+            Trabalho.find({user: req.body.user})
+                .then(trabalhos => {
+                    if (trabalhos.length === MAXIMO_TRABALHOS_POR_USUARIO) {
+                        res.status(400).json({message: `É permitido apenas submeter ${MAXIMO_TRABALHOS_POR_USUARIO} trabalhos por cadastro!`});
+                        return false;
                     }
+
+                    Trabalho.create(req.body)
+                        .then(trabalho => {
+                            if (req.files && req.files.file) {
+                                let file = req.files.file;
+                                TrabalhoAnexo.create({file: file.data, contentType: file.mimetype, trabalho})
+                                    .then(trabalhoAnexo => {
+                                        res.status(201).json(trabalho);
+                                    });
+                            } else {
+                                res.status(201).json(trabalho);
+                            }
+                        });
                 });
         },
         getAnexo: (req, res) => {
